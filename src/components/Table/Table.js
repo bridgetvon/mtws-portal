21da83132f18be1a8e91558fbc5c396e06e72235
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2023-07-25 15:55:24 -0600

    added radoi buttons to table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,102 +1,419 @@
 import Table from 'react-bootstrap/Table';
-import React from 'react';
+import React, { useState } from 'react';
 import MyForm from '../Form/Form';
 import './Table.css';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
 function MyTable() {
+  const [radioValue, setRadioValue] = useState('1');
+
+  const radios = [
+    { name: 'Active', value: '1' },
+    { name: 'Closed', value: '3' },
+  ];
+
   return (
     <div id="custom-table">
-    <Table variant="dark">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Submitted By</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Some pretend issue</td>
-          <td> Batman</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>another pretend issue</td>
-          <td>Tom</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>another pretend issue</td>
-          <td>Brian</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>another pretend issue</td>
-          <td>Andrew</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>another pretend issue</td>
-          <td>Adam</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>another pretend issue</td>
-          <td>Adam</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>another pretend issue</td>
-          <td>Adam</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>another pretend issue</td>
-          <td>Adam</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>another pretend issue</td>
-          <td>Adam</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>another pretend issue</td>
-          <td>Thor</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>another pretend issue</td>
-          <td>Loki</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>another pretend issue</td>
-          <td>Thor</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>another pretend issue</td>
-          <td>Thor</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>another pretend issue</td>
-          <td>Thor</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>another pretend issue</td>
-          <td>Thor</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>another pretend issue</td>
-          <td>Thor</td>
-        </tr>
-      </tbody>
-    </Table>
+      <Table variant="dark">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Submitted By</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Some pretend issue</td>
+            <td> Batman</td>
+            <td>
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>another pretend issue</td>
+            <td>Tom</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>another pretend issue</td>
+            <td>Brian</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>another pretend issue</td>
+            <td>Andrew</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>another pretend issue</td>
+            <td>Adam</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>another pretend issue</td>
+            <td>Adam</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>another pretend issue</td>
+            <td>Adam</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>another pretend issue</td>
+            <td>Adam</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>another pretend issue</td>
+            <td>Adam</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>another pretend issue</td>
+            <td>Thor</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>another pretend issue</td>
+            <td>Loki</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>another pretend issue</td>
+            <td>Thor</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>another pretend issue</td>
+            <td>Thor</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>another pretend issue</td>
+            <td>Thor</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>another pretend issue</td>
+            <td>Thor</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+          <tr>
+            <td>6</td>
+            <td>another pretend issue</td>
+            <td>Thor</td>
+            <td>
+
+              <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+              </ButtonGroup>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
