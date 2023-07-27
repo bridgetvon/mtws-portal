cd8eeeb51257dbf2ecfb9602320f3060d6b13209
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2023-07-27 14:57:20 -0600

    Updates

---
+++ precache-manifest.64527f9855759101b1e518e8d34632f1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "347365c1cf360d883b2c",
+    "url": "/static/css/main.9d87cd1b.chunk.css"
+  },
+  {
+    "revision": "347365c1cf360d883b2c",
+    "url": "/static/js/main.38983b25.chunk.js"
+  },
+  {
+    "revision": "edf1fe1021dd606bd408",
+    "url": "/static/js/runtime~main.24d23366.js"
+  },
+  {
+    "revision": "ca476f4d38f04e56ee0c",
+    "url": "/static/css/2.2714000d.chunk.css"
+  },
+  {
+    "revision": "ca476f4d38f04e56ee0c",
+    "url": "/static/js/2.5838305a.chunk.js"
+  },
+  {
+    "revision": "1d7db9e74b11237af689",
+    "url": "/static/js/3.d4561765.chunk.js"
+  },
+  {
+    "revision": "596631e04b36a290cc90e3bc782e245f",
+    "url": "/static/media/MTWSLogo.596631e0.png"
+  },
+  {
+    "revision": "0cf58f029e60d5e22ac7913062d47720",
+    "url": "/static/media/HelpDesk.0cf58f02.png"
+  },
+  {
+    "revision": "f87043c503074aa29fc593a4ee587792",
+    "url": "/static/media/Boat.f87043c5.png"
+  },
+  {
+    "revision": "db74589197fda7d819fdf36c23c9a5b0",
+    "url": "/static/media/Military.db745891.jpg"
+  },
+  {
+    "revision": "492261cc4c30dc1bc15dbc346e7451d1",
+    "url": "/static/media/Soldiers.492261cc.jpg"
+  },
+  {
+    "revision": "3c2eecbdd5c2c35028f5aa4974b72db0",
+    "url": "/static/media/Soldier.3c2eecbd.png"
+  },
+  {
+    "revision": "310250d51c20af1ca84ecd75b8b06d71",
+    "url": "/static/media/Resources.310250d5.png"
+  },
+  {
+    "revision": "ae47607ff65418c1141668a4ef82372c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mtws-portal/precache-manifest.4546a4a086532808f2b20a4ab88310dc.js"
+  "/precache-manifest.64527f9855759101b1e518e8d34632f1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/mtws-portal/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
