b2accce792f6e49affd1c13a470eae54d2923da2
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2023-08-03 10:46:46 -0600

    Updates

---
+++ precache-manifest.0c3b9b5f27f10167e46da2a0303bc803.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9f3152ef2650456457c",
+    "url": "/home/static/css/main.9963c618.chunk.css"
+  },
+  {
+    "revision": "e9f3152ef2650456457c",
+    "url": "/home/static/js/main.7c4b8f0a.chunk.js"
+  },
+  {
+    "revision": "4a6e6a441f3e98620d4c",
+    "url": "/home/static/js/runtime~main.d32fcd3b.js"
+  },
+  {
+    "revision": "67648450550cd73a2428",
+    "url": "/home/static/css/2.2714000d.chunk.css"
+  },
+  {
+    "revision": "67648450550cd73a2428",
+    "url": "/home/static/js/2.a940db52.chunk.js"
+  },
+  {
+    "revision": "1d7db9e74b11237af689",
+    "url": "/home/static/js/3.d4561765.chunk.js"
+  },
+  {
+    "revision": "596631e04b36a290cc90e3bc782e245f",
+    "url": "/home/static/media/MTWSLogo.596631e0.png"
+  },
+  {
+    "revision": "0cf58f029e60d5e22ac7913062d47720",
+    "url": "/home/static/media/HelpDesk.0cf58f02.png"
+  },
+  {
+    "revision": "f87043c503074aa29fc593a4ee587792",
+    "url": "/home/static/media/Boat.f87043c5.png"
+  },
+  {
+    "revision": "db74589197fda7d819fdf36c23c9a5b0",
+    "url": "/home/static/media/Military.db745891.jpg"
+  },
+  {
+    "revision": "492261cc4c30dc1bc15dbc346e7451d1",
+    "url": "/home/static/media/Soldiers.492261cc.jpg"
+  },
+  {
+    "revision": "3c2eecbdd5c2c35028f5aa4974b72db0",
+    "url": "/home/static/media/Soldier.3c2eecbd.png"
+  },
+  {
+    "revision": "310250d51c20af1ca84ecd75b8b06d71",
+    "url": "/home/static/media/Resources.310250d5.png"
+  },
+  {
+    "revision": "958c570343303fed1968154c5924de6c",
+    "url": "/home/index.html"
+  }
+];
