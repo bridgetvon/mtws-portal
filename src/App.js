c46add8c68c58e8c653ac996ebd280d8e759aace
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2023-08-03 14:35:35 -0600

    hashrouter?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Hero from './components/Hero/Hero';
 import Login from './components/Login/Login';
@@ -29,7 +29,7 @@ function App() {
           <Navbar toggle={toggle} />
           {isOpen ? < Sidebar toggle={toggle} /> : ''}
           <Routes>
-            <Route path="/" element={<Hero />} />
+            <Route path="/home" element={<Hero />} />
             <Route path="/login" element={<Login />} />
             <Route path="/helpDesk" element={<HelpDesk />} />
             <Route path="/forum" element={<Forum />} />
