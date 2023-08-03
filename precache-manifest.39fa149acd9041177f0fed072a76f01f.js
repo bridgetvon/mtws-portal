b5582dafd449498e251eed008368a0888f9cb781
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2023-08-03 11:00:07 -0600

    Updates

---
+++ precache-manifest.39fa149acd9041177f0fed072a76f01f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e4a63bef8cacc9ead6b",
+    "url": "/mtws-portal/static/css/main.bb951b69.chunk.css"
+  },
+  {
+    "revision": "4e4a63bef8cacc9ead6b",
+    "url": "/mtws-portal/static/js/main.24850cc9.chunk.js"
+  },
+  {
+    "revision": "35be2c9def4e873e4128",
+    "url": "/mtws-portal/static/js/runtime~main.bb5a417f.js"
+  },
+  {
+    "revision": "e661c69c39bb979c5979",
+    "url": "/mtws-portal/static/css/2.2714000d.chunk.css"
+  },
+  {
+    "revision": "e661c69c39bb979c5979",
+    "url": "/mtws-portal/static/js/2.cf0273a1.chunk.js"
+  },
+  {
+    "revision": "1d7db9e74b11237af689",
+    "url": "/mtws-portal/static/js/3.d4561765.chunk.js"
+  },
+  {
+    "revision": "596631e04b36a290cc90e3bc782e245f",
+    "url": "/mtws-portal/static/media/MTWSLogo.596631e0.png"
+  },
+  {
+    "revision": "0cf58f029e60d5e22ac7913062d47720",
+    "url": "/mtws-portal/static/media/HelpDesk.0cf58f02.png"
+  },
+  {
+    "revision": "f87043c503074aa29fc593a4ee587792",
+    "url": "/mtws-portal/static/media/Boat.f87043c5.png"
+  },
+  {
+    "revision": "db74589197fda7d819fdf36c23c9a5b0",
+    "url": "/mtws-portal/static/media/Military.db745891.jpg"
+  },
+  {
+    "revision": "492261cc4c30dc1bc15dbc346e7451d1",
+    "url": "/mtws-portal/static/media/Soldiers.492261cc.jpg"
+  },
+  {
+    "revision": "3c2eecbdd5c2c35028f5aa4974b72db0",
+    "url": "/mtws-portal/static/media/Soldier.3c2eecbd.png"
+  },
+  {
+    "revision": "310250d51c20af1ca84ecd75b8b06d71",
+    "url": "/mtws-portal/static/media/Resources.310250d5.png"
+  },
+  {
+    "revision": "f12d288cbd9fc559093bfe49cb11dc4c",
+    "url": "/mtws-portal/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2958528c3b0733b44c0d10fd008462c.js"
+  "/mtws-portal/precache-manifest.39fa149acd9041177f0fed072a76f01f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mtws-portal/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
