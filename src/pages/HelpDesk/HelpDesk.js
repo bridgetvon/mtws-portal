637407c65d6439ab090de749b652297a6ce2027c
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2023-08-03 07:15:20 -0600

    urg

--- src/pages/HelpDesk/HelpDesk.js
+++ src/pages/HelpDesk/HelpDesk.js
@@ -25,10 +25,10 @@ function HelpDesk() {
                     <Col lg={2} className="mt-3">
                         <Stack>
                             <Form.Control className="me-auto" id="search-input" placeholder="Search for..." />
-                            <Button variant="outline-light">
+                            <Button id='help-btn' variant="outline-light">
                                 <FaSearch style={{ marginRight: '5px', marginBottom: '2px', marginTop:'3px' }}  />
                                 Search</Button>
-                            <Button variant="outline-success" onClick={handleShow}>
+                            <Button id='help-btn' variant="outline-success" onClick={handleShow}>
                                 <FaRegEdit style={{ marginRight: '5px' }} />
                                 New help ticket
                             </Button>
@@ -44,15 +44,15 @@ function HelpDesk() {
                                     </Card>
                                 </Modal.Body>
                                 <Modal.Footer style={{ border: '1px solid #fff', background: '#2a2a2a' }}>
-                                    <Button variant="outline-danger" onClick={handleClose}>
+                                    <Button id='help-btn' variant="outline-danger" onClick={handleClose}>
                                         Close
                                     </Button>
-                                    <Button variant="outline-success" onClick={handleClose}>
+                                    <Button  id='help-btn' variant="outline-success" onClick={handleClose}>
                                         Save Changes
                                     </Button>
                                 </Modal.Footer>
                             </Modal>
-                            <Button variant="outline-secondary" onClick={handleShow}>
+                            <Button id='help-btn' variant="outline-secondary" onClick={handleShow}>
                                 <FaCheck style={{ marginRight: '5px' }}/>
                                 New Action Item
                             </Button>
@@ -76,7 +76,7 @@ function HelpDesk() {
                                     </Button>
                                 </Modal.Footer>
                             </Modal>
-                            <Button variant="outline-primary" onClick={handleShow}>
+                            <Button  id='help-btn' variant="outline-primary" onClick={handleShow}>
                                 <FaRegFlag style={{ marginRight: '7px' }} />
                                 New Update
                             </Button>
@@ -100,15 +100,15 @@ function HelpDesk() {
                                     </Button>
                                 </Modal.Footer>
                             </Modal>
-                            <Button variant="outline-danger" onClick={handleShow}>
+                            <Button id='help-btn' variant="outline-danger" onClick={handleShow}>
                                 <AiFillBug style={{ marginRight: '5px' }}/>
                                Bug Report
                             </Button>
-                            <Button variant="outline-warning" onClick={handleShow}>
+                            <Button id='help-btn' variant="outline-warning" onClick={handleShow}>
                                 <FaExchangeAlt style={{ marginRight: '5px' }}/>
                                 Change Request
                             </Button>
-                            <Button variant="outline-info" onClick={handleShow}>
+                            <Button id='help-btn' variant="outline-info" onClick={handleShow}>
                                 <BiSupport style={{ marginRight: '5px' }}/>
                                 Tech Support
                             </Button>
